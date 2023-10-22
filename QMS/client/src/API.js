f7c1f2a12d3aa55388ee7ed32920e4ce7eac08f8
LORIS FABRIZIO MARIO VITALE S317264 <s317264@studenti.polito.it>
2023-10-22 18:08:50 +0200

    created API.js client for CORS and added sessions apis

---
+++ QMS/client/src/API.js
@@ -0,0 +1,45 @@
+const SERVER_URL = 'http://localhost:3001';
+
+
+const logIn = async (credentials) => {
+  const response = await fetch(SERVER_URL + '/api/sessions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(credentials),
+  });
+  if (response.ok) {
+    const user = await response.json();
+    return user;
+  }
+  else {
+    const errDetails = await response.text();
+    throw errDetails;
+  }
+};
+
+const getUserInfo = async () => {
+  const response = await fetch(SERVER_URL + '/api/sessions/current', {
+    credentials: 'include',
+  });
+  const user = await response.json();
+  if (response.ok) {
+    return user;
+  } else {
+    throw user;  // an object with the error coming from the server
+  }
+};
+
+const logOut = async () => {
+  const response = await fetch(SERVER_URL + '/api/sessions/current', {
+    method: 'DELETE',
+    credentials: 'include'
+  });
+  if (response.ok)
+    return null;
+}
+
+const API = { logIn, logOut, getUserInfo };
+export default API;
