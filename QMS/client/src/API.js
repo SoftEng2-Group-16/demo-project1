27c6fa26a8a2acf99261634c880702cd3ba3c2ad
fraR-coder <francesco.mail5.0@gmail.com>
2023-10-24 14:48:46 +0200

    fixed getticket

--- QMS/client/src/API.js
+++ QMS/client/src/API.js
@@ -62,36 +62,16 @@ async function createNewTicket(serviceType) {
                 "ts":ts,
                 "status":status
               }
-
-
-  const response = await fetch(SERVER_URL + '/api/ticket', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-    body: JSON.stringify(ticket),
-  });
-  if (response.ok) {
-    const ticket = await response.json();
-    return ticket;
-  }
-  else {
-    const errDetails = await response.text();
-    throw errDetails;
-  }
-};
-  
-/*   return getJson(fetch(SERVER_URL + '/api/ticket'), {
+  return getJson((fetch(SERVER_URL + '/api/ticket'), {
     method:'POST',
     body: JSON.stringify(ticket),
     headers: {
       'Content-Type': 'application/json',
     },
     
-  }) */
+  }))
   
-
+}
 
 
 
@@ -101,7 +81,6 @@ async function createNewTicket(serviceType) {
  * A utility function for parsing the HTTP response.
  */
 function getJson(httpResponsePromise) {
-  console.log(httpResponsePromise)
   // server API always return JSON, in case of error the format is the following { error: <message> } 
   return new Promise((resolve, reject) => {
     httpResponsePromise
