4c5959c21b33275e6ea7d07c2ab7be7800c9bf91
kat-maq <laimirandasantos@gmail.com>
2023-10-26 16:59:25 +0100

    Add finish service button on the employee page

--- QMS/client/src/components/Employee.jsx
+++ QMS/client/src/components/Employee.jsx
@@ -9,6 +9,7 @@ function EmployeePage(props) {
   const { handleErrors } = useContext(MessageContext);
   const [services, setServices] = useState([]); // List of services with their information
   const [loading, setLoading] = useState(true);
+  const [ticketID, setTicketID] = useState(null);
   const [buttonsVisible, setButtonsVisible] = useState(false);
 
   useEffect(() => {
@@ -17,6 +18,7 @@ function EmployeePage(props) {
       .then((services) => {
         setServices(services);
         setLoading(false);
+        setTicketID(2);
       })
       .catch((err) => {
         handleErrors(err);
@@ -25,8 +27,17 @@ function EmployeePage(props) {
 
   const handleNextCustomer = (service) => {
     // Handle the action when Button 1 is clicked
-
+    
   };
+
+  const handleFinishService = (ticketID) => {
+    API.closeTicket(ticketID).then((res) => {
+      console.log(res)
+      // TODO: disable finish service button and enable call next customer button
+      }).catch((err) => {
+      handleErrors(err);
+    });
+  }
 
   return (
     <>
@@ -38,15 +49,16 @@ function EmployeePage(props) {
             {services.map((service) => (
               <Col key={service.id}>
                 <Card style={{ width: '18rem', height: '12rem', marginBottom: '20px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)' }}>
-                  <Card.Body>
+                  <Card.Body>LoadingLayout
                     <Card.Title style={{ fontSize: '1.5rem', fontWeight: 'bold', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'normal' }}>
                       {service.type}
                     </Card.Title>
                     <Card.Text style={{ fontSize: '1rem', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'normal' }}>
                       {service.description}
                     </Card.Text>
-                    <div style={{ marginTop: '10px' }}>
-                      <Button variant="primary" style={{ marginRight: '10px' }} onClick={() => handleNextCustomer(service)}>Call Next Customer</Button>
+                    <div className="d-flex justify-content-around" style={{ marginTop: '10px' }}>
+                      <Button variant="primary" style={{ marginRight: '10px'}} onClick={() => handleNextCustomer(service)}>Call Next Customer</Button>
+                      <Button variant="secondary" style={{ marginRight: '10px' }} onClick={() => handleFinishService(ticketID)}>Finish Service</Button>
                       {/* Add more buttons if necessary */}
                     </div>
                   </Card.Body>
